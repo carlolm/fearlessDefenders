7f4639b8038fe12d7492a26f70c7ea277251b30a
Vaggelis Sotiropoulos <vsotiropoulo@gmail.com>
2017-05-02 16:30:15 -0700

    (feat) Conforms to style guide

--- server/server.js
+++ server/server.js
@@ -1,39 +1,39 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
+const app = express();
 
 // Cors dealt with outside server.js although kept commented if needed
-var headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-  "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE"
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+  'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE',
 };
 
-app.use(express.static(__dirname + '/../src'));
+app.use(express.static(__dirname.join('/../src')));
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header(headers);
   next();
 });
 
 app.use(cookieParser());
 app.use(bodyParser());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 
-app.post('/', function(req, res) {
+app.post('/', (req, res) => {
   res.writeHead(201, headers);
   res.end('Hello World');
 });
 
 // To add when deployed
-	// app.get('/cookie', function(req, res) {
-	//   res.cookie(cookie_name , 'cookie_value').send('Cookie is set');
-	// });
+// app.get('/cookie', function(req, res) {
+//   res.cookie(cookie_name , 'cookie_value').send('Cookie is set');
+// });
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.writeHead(200, headers);
   res.end('Hello Get World');
 });
