0cff4acff6371244c6075ddedf2e37d41e53904e
calebcordry <caleb.cordry@gmail.com>
2017-05-03 15:47:44 -0700

    Hook up watson`

--- server/server.js
+++ server/server.js
@@ -2,62 +2,34 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const path = require('path');
-const toneAnalyzer = require('../watson');
+
+const toneAnalyzer = require('./watson');
 
 const app = express();
 
-// Cors dealt with outside server.js although kept commented if needed
-const headers = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-  'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE',
-};
-
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.use((req, res, next) => {
-  res.header(headers);
-  next();
-});
-
 app.use(cookieParser());
-app.use(bodyParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 
-app.post('/', (req, res) => {
-  res.writeHead(201, headers);
-  res.end('Hello World');
-});
+app.get('/api/watson', (req, res) => {
+  const input = {
+    utterances: [
+      { text: 'Basically think of $AAPL as a ~$32B SaaS business linked to a ~$200B hardware business with gross margins of ~38 percent ' },
+      { text: 'When @tim_cook was here one year ago today, $AAPL was at $93. "Hold it, don\'t trade it." -@JimCramer' },
+      { text: 'Hilarious that @jimcramer mentioned the @amazonfirephone in his interview with @tim_cook. #CNBC #MadMoney $AAPL' },
+    ],
+  };
 
-// To add when deployed
-// app.get('/cookie', function(req, res) {
-//   res.cookie(cookie_name , 'cookie_value').send('Cookie is set');
-// });
-
-app.get('/', (req, res) => {
-  res.status(200).sendFile('./index.html');
-});
-
-/*watson requests*/
-
-app.get('/v3/tone', (req, res) => {
-
-});
-
-app.post('/v3/tone', (req, res) => {
-  toneAnalyzer.tone({ text: 'A word is dead when it is said, some say. Emily Dickinson' },
-    function(err, tone) {
-      if (err)
-        console.log(err);
-      else
-        res.json(tone, null, 2);
+  toneAnalyzer.tone(input, (err, tone) => {
+    if (err) { return console.warn(err); }
+    return res.send(tone);
   });
 });
 
-app.get('*', (req, res) => {
-  res.writeHead(404, headers);
-  res.end('Resource not found');
+app.get('/:bad', (req, res) => {
+  res.status(404).send(`Resource not found ${req.params.bad}`);
 });
 
 app.listen(process.env.PORT || 3000);
---
+++ server/watson.js
@@ -0,0 +1,12 @@
+const watson = require('watson-developer-cloud');
+const keys = require('./config');
+
+const toneAnalyzer = watson.tone_analyzer({
+  username: keys.watson.username,
+  password: keys.watson.username,
+  version: 'v3',
+  version_date: '2016-05-19 ',
+});
+
+
+module.exports = toneAnalyzer;
