304330e825ef45462a73f2440f5938e2e643366c
Michael Kass <Michael Kass>
2017-05-02 19:33:00 -0700

    in progress for watson

---
+++ fakeData/watsonMockData.js
@@ -0,0 +1,13 @@
+module.exports = {
+  oneUser: {
+    "text": "A word is dead when it is said, some say. Emily Dickinson"
+  },
+  multiUser: {
+    utterances: [
+        {"text": "Hello, can you help me", "user": "customer"},
+        {"text": "How are you ?", "user": "agent"},
+        {"text": "Nothing is working" :, "user": "customer"},
+        {"text": "Sorry to hear this", "user": "agent"}
+    ]
+  }
+}
