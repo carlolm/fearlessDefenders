b39f792b0e7cfe5f450ff1e0ce2090a38161a3d1
Carlo Las Marias <carlol@gmail.com>
2017-05-03 23:08:01 -0700

    Twitter function complete

--- spikes/twitter-node-client.js
+++ spikes/twitter-node-client.js
@@ -1,30 +1,6 @@
 const Twitter = require('twitter-node-client').Twitter;
-const Keys = require('./twitterKeys.js');
-
-const error = function (err, response, body) {
-  console.log(body);
-  console.log('ERROR [%s]', err);
-};
-const success = function (data) {
-  // console.log('Data [%s]', data);
-  let tweets = JSON.parse(data);
-
-  if (!Array.isArray(tweets) && tweets.statuses && Array.isArray(tweets.statuses)) {
-    tweets = tweets.statuses;
-  }
-
-  if (Array.isArray(tweets)) {
-    tweets.forEach((tweet) => {
-      console.log('------- ', tweet.created_at);
-      // console.log('@', tweet.user.screen_name, '(', tweet.user.name, ')');
-      console.log('=> ', tweet.text);
-    });
-
-    console.log('---- SIZE OF TWEETS: ', tweets.length);
-  } else {
-    console.log(typeof tweets, Object.keys(tweets.statuses), Array.isArray(tweets.statuses));
-  }
-};
+const Keys = require('./twitter_keys.js');
+const Promise = require('bluebird');
 
 const keys = {
   consumerKey: Keys.consumer_key,
@@ -35,35 +11,134 @@ const keys = {
 
 const twitter = new Twitter(keys);
 
-// const params = {
-//   screen_name: 'realDonaldTrump',
-//   count: '10',
-// };
+/**
+ * Helper function that loops through promises (getting tweets)
+ * until the while condition evaluates to false.
+ * @function
+ * @param {function} condition - a function that evalutes to a boolean
+ * @param {function} action - a promise function whose evaluation is looped
+ */
 
-// twitter.getUserTimeline(params, error, success);
+const promiseWhile = (condition, action) => {
+  const resolver = Promise.defer();
 
-// twitter.getMentionsTimeline({ count: '10' }, error, success);
+  const loop = () => {
+    if (!condition()) return resolver.resolve();
+    return Promise.cast(action())
+      .then(loop)
+      .catch(resolver.reject);
+  };
 
-// twitter.getHomeTimeline({ count: '10' }, error, success);
+  process.nextTick(loop);
 
-// twitter.getReTweetsOfMe({ count: '10' }, error, success);
-
-//
-// Get 10 tweets containing the hashtag haiku
-//
-
-// search?l=&q=hack%20reactor%20since%3A2017-03-26%20until%3A2017-03-27&src=typd
-
-const params = {
-  q: '$AAPL stock',
-  count: 100,
+  return resolver.promise;
 };
 
-twitter.getSearch(params, error, success);
+/**
+ * Get an array of tweet objects
+ * @function
+ * @param {string} query - Query string (e.g. '$AAPL' for apple stock ticker)
+ * @param {string} startDate - earliest date of tweets to retrieve 'yyyy-mm-dd'
+ * @param {string} endDate - latest date of tweets to retrieve 'yyyy-mm-dd'
+ *                           Note: up to and *excluding* this date
+ *                           For current tweets, use current date + 1 day
+ * @param {integer} maxTweets - max number of tweets to retrieve
+ * @param {function} callback - function that takes in the resulting tweetSet as
+ *                              a parameter
+ */
 
-//
-// Get 10 popular tweets with a positive attitude about a movie that is not scary.
-//
 
-// twitter.getSearch({ 'q': ' movie -scary :)ls
-// since:2013-12-27', 'count': 10, 'result\_type': 'popular' }, error, success);
+const getTweetSet = (query, startDate, endDate, maxTweets, callback) => {
+
+  let tweetSet = [];
+  let tweetCount = 0;
+  let endOfResults = false;
+
+  // Last tweet retrieved from current "page" of retrieved tweets
+  // = the starting point for the next call to retrieve tweets
+  let maxId = null;
+
+  const condition = () => {
+    let reachedStartDate = false;
+    if (tweetSet.length > 0) {
+      const start = new Date(startDate);
+      const oldestTweetDate = new Date(tweetSet[tweetSet.length - 1].created_at);
+      console.log(' reachedStartDate: ', oldestTweetDate < start, oldestTweetDate, start);
+      reachedStartDate = oldestTweetDate < start;
+    }
+    console.log('Conditions: length ', tweetSet.length < maxTweets, ' | !endOfResults ', !endOfResults, ' | !reachedStartDate ', !reachedStartDate);
+    return tweetSet.length < maxTweets && !endOfResults && !reachedStartDate;
+  };
+
+  promiseWhile(condition, () => {
+    return new Promise((resolve, reject) => {
+      let tweetsArray = [];
+
+      const params = {
+        q: query,
+        until: endDate,
+        count: 100,
+      };
+
+      if (maxId !== null) params.max_id = maxId;
+
+      // Callback function for error case for twitter.getSearch
+      const error = (err, response, body) => {
+        console.log('ERROR');
+        console.log(body);
+        console.log('ERROR [%s]', err);
+        endOfResults = true;
+        reject([]);
+      };
+
+      // Callback function for success case for twitter.getSearch()
+      // Retrieves maxium 100 tweets per query
+      const success = (data) => {
+        let tweets = JSON.parse(data);
+
+        if (!Array.isArray(tweets) && tweets.statuses && Array.isArray(tweets.statuses)) {
+          tweets = tweets.statuses;
+        }
+
+        if (Array.isArray(tweets)) {
+          if (tweets.length === 0) {
+            endOfResults = true;
+          } else {
+            tweetsArray = tweets.map((tweet) => {
+              return {
+                id: tweet.id,
+                created_at: tweet.created_at,
+                text: tweet.text,
+              };
+            });
+            tweetSet = tweetSet.concat(tweetsArray);
+            maxId = tweetsArray[tweetsArray.length - 1].id;
+            tweetCount += tweetsArray.length;
+          }
+        } else {
+          console.warn('**** Result type is not an array ****');
+        }
+        resolve(tweetsArray);
+      };
+
+      twitter.getSearch(params, error, success);
+    });
+  })
+    .then(() => {
+      callback(tweetSet);
+    });
+};
+
+/**
+ *  Example usage:
+ */
+
+getTweetSet('$AAPL', '2017-03-01', '2017-05-03', 200, (tweetSet) => {
+  console.log('*** Tweets received *** [', tweetSet.length, ']');
+
+  if (tweetSet.length > 0) {
+    console.log('Most recent: ', tweetSet[0].created_at);
+    console.log('Least recent: ', tweetSet[tweetSet.length - 1].created_at);
+    console.log(tweetSet.slice(190));
+  }
+});
