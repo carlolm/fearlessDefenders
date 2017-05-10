6ecd220ecde8c692584d1f50e99ecc0e559a0862
Carlo Las Marias <carlol@gmail.com>
2017-05-10 15:52:16 -0700

    watson render fix

--- src/components/WatsonSummary.js
+++ src/components/WatsonSummary.js
@@ -3,6 +3,8 @@ import Chart from 'chart.js';
 
 import './css/WatsonSummary.css';
 
+let ctx;
+let barChart;
 
 class WatsonSummary extends Component {
   constructor(props) {
@@ -15,47 +17,55 @@ class WatsonSummary extends Component {
 
   generateChart() {
 
-    const labels = this.props.companiesSummary.map(tuple => tuple[0]);
-    const data = this.props.companiesSummary.map(tuple => tuple[2] * 100);
+    console.log('*** companiesSummary length: ', this.props.companiesSummary.length);
 
-    let axisMax = Math.max(Math.max.apply(null, data), -Math.min.apply(null, data));
-    axisMax = Math.min(100, Math.ceil(axisMax / 100 * 4) * 25);
+    if (this.props.companiesSummary.length !== 0) {
 
-    const colors = data.map((score) => {
-      if (score >= 0) {
-        return `rgba(75, 192, 192, ${Math.max(score/axisMax, 0.25)})`
-      } else {
-        return `rgba(255, 99, 132, ${Math.max(-score/axisMax, 0.25)})`
-      }
-    });
+      const labels = this.props.companiesSummary.map(tuple => tuple[0]);
+      const data = this.props.companiesSummary.map(tuple => tuple[2] * 100);
 
-    const ctx = document.getElementById('watson-chart');
-    const barChart = new Chart(ctx, {
-      type: 'horizontalBar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Watson Score',
-          data: data,
-          backgroundColor: colors,
-          borderColor: colors,
-          borderWidth: 1,
-        }],
-      },
-      options: {
-        scales: {
-          xAxes: [{
-            ticks: {
-              min: -axisMax, // Edit the value according to what you need
-              max: axisMax,
-            },
-          }],
-          yAxes: [{
-            stacked: true,
+      let axisMax = Math.max(Math.max.apply(null, data), -Math.min.apply(null, data));
+      axisMax = Math.min(100, Math.ceil(axisMax / 100 * 4) * 25);
+
+      const colors = data.map((score) => {
+        if (score >= 0) {
+          return `rgba(75, 192, 192, ${Math.max(score / axisMax, 0.25)})`;
+        } else {
+          return `rgba(255, 99, 132, ${Math.max(-score / axisMax, 0.25)})`
+        }
+      });
+
+      ctx = document.getElementById('watson-chart');
+
+      if (barChart !== undefined) barChart.destroy();
+
+      barChart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: 'Watson Score',
+            data: data,
+            backgroundColor: colors,
+            borderColor: colors,
+            borderWidth: 1,
           }],
         },
-      },
-    });
+        options: {
+          scales: {
+            xAxes: [{
+              ticks: {
+                min: -axisMax, // Edit the value according to what you need
+                max: axisMax,
+              },
+            }],
+            yAxes: [{
+              stacked: true,
+            }],
+          },
+        },
+      });
+    }
   }
 
   componentDidMount() {
