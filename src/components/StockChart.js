17453afeae6bc1373e908d60a5061623600edcd1
Carlo Las Marias <carlol@gmail.com>
2017-05-15 15:55:39 -0700

    reverted

--- src/components/StockChart.js
+++ src/components/StockChart.js
@@ -30,29 +30,19 @@ class StockChart extends Component {
 
     console.log('** STOCK CHART - createChart: ', ticker);
 
-    $.ajax({
+     $.ajax({
       type: 'GET',
       url: `/api/quandl/${ticker}`,
       success: (result) => {
         if (this.props.data.length !== 0) {
-          const json = JSON.parse(result);
+          result = JSON.parse(result);
           const labels = [];
           const quandlData = [];
           const watsData = [];
-          var j = 0;
-          console.log('wats data: ', this.props.data);
-          for (var i = 0; i < 5; i++) {
-            labels.unshift(json.dataset.data[i][0]);
-            quandlData.unshift(json.dataset.data[i][4]);
-            console.log('j: ', j);
-            while(this.props.data[j] !== undefined && this.props.data[j].date !== json.dataset.data[i][0]) {
-              j += 1;
-              // console.log(this.props.data[j]);
-              console.log('wats date: ', this.props.data[j].date || '[StockChart] ** watson date is null **');
-              console.log('quandl date: ', json.dataset.data[i][0]);
-            }
-            watsData.unshift((this.props.data[j].score * 100));
-            j += 1;
+          for (let i = 0; i < 5; i++) {
+            labels.unshift(result.dataset.data[i][0]);
+            quandlData.unshift(result.dataset.data[i][4]);
+            watsData.unshift((this.props.data[i].score * 100));
           }
           const ctx = document.getElementById('stock-chart');
 
@@ -64,12 +54,12 @@ class StockChart extends Component {
           myChart = new ChartJS(ctx, {
             type: 'bar',
             data: {
-              labels,
+              labels: labels,
               datasets: [
                 {
                   type: 'bar',
                   label: 'Watson Score',
-                  yAxisID: 'y-axis-0',
+                  yAxisID: "y-axis-0",
                   data: watsData,
                   backgroundColor: [
                     'rgba(0, 99, 132, 0.2)',
@@ -90,33 +80,40 @@ class StockChart extends Component {
                   borderWidth: 1,
                 },
                 {
-                  type: 'line',
-                  label: 'Stock Price',
-                  yAxisID: 'y-axis-1',
-                  data: quandlData,
-                  borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                  ],
-                  borderWidth: 1,
-                }],
+                type: 'line',
+                label: 'Stock Price',
+                yAxisID:"y-axis-1",
+                data: quandlData,
+                // backgroundColor: [
+                //   'rgba(255, 99, 132, 0.2)',
+                //   'rgba(54, 162, 235, 0.2)',
+                //   'rgba(255, 206, 86, 0.2)',
+                //   'rgba(75, 192, 192, 0.2)',
+                //   'rgba(153, 102, 255, 0.2)',
+                //   'rgba(255, 159, 64, 0.2)',
+                // ],
+                borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+              }],
             },
             options: {
               scales: {
                 yAxes: [{
-                  position: 'left',
-                  id: 'y-axis-0',
-                },
-                {
-                  position: 'right',
-                  id: 'y-axis-1',
-                }],
+                  position: "left",
+                  "id": "y-axis-0"
+                  }, {
+                  position: "right",
+                  "id": "y-axis-1"
+                }]
               },
-            },
+            }
           });
         }
       },
@@ -127,12 +124,16 @@ class StockChart extends Component {
     });
   }
 
+  componentDidUpdate() {
+    this.updateChart();
+  }
+ 
   render() {
     return (
       <div className="chart-container">
-        { (!this.props.ticker) ?
-          <h2> Enter a stock to begin </h2> :
-          <h2>{this.props.ticker} Stock Price</h2> }
+
+        { (this.props.ticker === 'none' ) ? <h2> Enter a stock to begin </h2> : 
+        <h2>{this.props.ticker} Stock Price</h2> }
         <canvas className="center" height="300px" id="stock-chart" />
       </div>
     );
